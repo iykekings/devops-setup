a38fab52c8c05d3a3b7c50aee9fa5f2149d384a4
Ikechukwu <iykekings36@gmail.com>
2019-08-19 00:53:46 +0100

    changed pre-commit hook to lint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sum(...numbers) {
   if (numbers.every(n => typeof n === 'number')) {
     return numbers.reduce((acc, item) => acc + item, 0);
   }
-  throw new Error('Contains invalid number');
+  throw new Error("Contains invalid number");
 }
 const person = {
   name: 'Matt',
