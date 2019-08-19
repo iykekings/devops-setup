aca8e655fe6ca24ec4c7fc8b4c592a088df7e79d
Ikechukwu <iykekings36@gmail.com>
2019-08-19 01:14:45 +0100

    more tests

--- index.test.js
+++ index.test.js
@@ -2,5 +2,12 @@ const { sum } = require('./index');
 
 test('should return sum of two numbers', () => {
   expect(sum(3, 5)).toBe(8);
+});
+
+test('should return sum of three numbers', () => {
   expect(sum(3, 4, 5)).toBe(12);
+});
+
+test('should return sum of spread array of numbers', () => {
+  expect(sum(...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(55);
 });
